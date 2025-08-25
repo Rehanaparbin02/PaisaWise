074033790c83957692625ee66308ddac24f55a2c
Rehana Parbin <rehanaparbin0210@gmail.com>
2025-08-25 15:32:16 +0530

    OCR setup

---
+++ app/components/OCR/ReceiptProcessor.jsx
@@ -0,0 +1,14 @@
+//This is the Optical Character Recognition (OCR) engine. It's a React Native wrapper for Google's ML Kit, which performs the core task of recognizing and extracting text from an image. 
+import TextRecognition from '@react-native-ml-kit/text-recognition'
+
+import { launchImageLibrary,launchCamera } from 'react-native-image-picker'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+class ReceiptProcessor {
+    constructor() {
+        this.categories = [
+            
+        ]
+    }
+}
+
